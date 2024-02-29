93afa23f56a8ee46bcf9ebfe2d405a11b1e0add7
JunJangE <wnswkd486@gmail.com>
2024-02-29 17:12:22 +0900

    feat: 자기소개 구현

---
+++ js/alert.js
@@ -0,0 +1,7 @@
+function showAlert() {
+    var name = document.getElementById('name').value;
+    var checkboxes = document.querySelectorAll('input[name="favorite_movie"]:checked');
+    var count = checkboxes.length;
+    var message = name + '님, 저와 ' + count + '개의 취향이 같으시네요!';
+    alert(message);
+}
